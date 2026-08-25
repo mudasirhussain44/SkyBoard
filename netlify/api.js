4ac8ceb025c92784e85ebe0239d99b40ef85f79c
Mudasir Hussain <khosomudasir786@gmail.com>
2026-08-25 00:11:07 -0700

    Delete netlify/api.js

--- netlify/api.js
+++
@@ -1,7 +0,0 @@
-// ================================================
-// Netlify Function — Express app ko serverless mein wrap karta hai
-// ================================================
-const serverless = require('serverless-http');
-const app = require('../../server');
-
-exports.handler = serverless(app);
